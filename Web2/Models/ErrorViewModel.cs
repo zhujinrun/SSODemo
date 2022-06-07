796966a8f011264c2edada8dc7318bfa594d3513
zhujinrun <13235578006@163.com>
2022-06-07 14:31:56 +0800

    集成 SSO（对称加密模式 ）到 Web2 站点

--- Web2/Models/ErrorViewModel.cs
+++ Web2/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Web2.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
---
+++ Web2/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web2.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
